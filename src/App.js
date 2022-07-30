7661cdcae9f37c3663b868384d7c9c37fdda5c3b
Edimar <edimar@Edimars-iMac-Pro.local>
2022-07-30 14:40:15 -0300

    React Fragment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 
 // Functional Component (Componente Funcional)
 function App() {
-  return <h1>Componente App</h1>;
+  return (
+    // Usar React.Fragment no lugar de dev/span/etc, assim nao aparece no source.
+    // pode usar <> também
+    <React.Fragment> 
+      <h1>Componente App</h1>
+      <h2>Sub</h2>
+    </React.Fragment>
+    
+  );
 }
 
 export default App;
