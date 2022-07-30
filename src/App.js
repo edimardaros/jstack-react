73ab729e26c2a644ca9c79752c146e45fe6c3db2
Edimar <edimar@Edimars-iMac-Pro.local>
2022-07-30 14:57:47 -0300

    O que sao Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from "react";
 
+import Post from './Post';
+
+const tituloZeroUm = 'Atividades 01'
+
 // Functional Component (Componente Funcional)
 function App() {
   return (
     // Usar React.Fragment no lugar de dev/span/etc, assim nao aparece no source.
     // pode usar <> também
     <React.Fragment> 
-      <h1>Componente App</h1>
-      <h2>Sub</h2>
+      <h1>Atividades</h1>
+      <h2>Atividades para fazer</h2>
+
+
+      <hr />
+
+      <Post
+        post={{
+          title: 'Titulo da Atividade 01',
+          subtitle: 'Subtitulo'
+        }}
+        />
+      
+      <Post
+        post={{
+          title: 'Titulo da Atividade 02',
+          subtitle: 'Subtitulo'
+        }}
+        />
+
+<Post
+        post={{
+          title: 'Titulo da Atividade 03',
+          subtitle: 'Subtitulo'
+        }}
+        />
+     
+      
+      {/* <Post
+        title="Titulo da Atividade 1"
+        subtitle="Subtitulo da Atividade"
+        /> */}
+      
     </React.Fragment>
     
   );
