793a9399d01fe2daa340b8c7eebc03d00c95943a
Edimar <edimar@Edimars-iMac-Pro.local>
2022-07-30 15:50:30 -0300

    Trabalhando com States

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Post from './Post';
 import Header from "./Header";
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 10},
-  { title: 'Title#02', subtitle: 'Sub#02', likes: 20},
-  { title: 'Title#03', subtitle: 'Sub#03', likes: 30},
-]
-
 // Functional Component (Componente Funcional)
 function App() {
+
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 10},
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 20},
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 30},
+  ]);
+  
+  function handleRefresh() {
+    // setPosts([]); // Limpar lista
+
+    setPosts((prevState) => [
+      ... prevState, 
+      {
+        id: Math.random(),
+        title: `Title#0${prevState.length + 1}`,
+        subtitle: `Sub#0${prevState.length + 1}`,
+        likes: 25,
+      }
+    ])
+  }
+
   return (
     // Usar React.Fragment no lugar de dev/span/etc, assim nao aparece no source.
     // pode usar <> também
     <React.Fragment> 
       
       <Header>
-        <h2>Atividades para fazer</h2>
+        <h2>Atividades para fazer
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
 
       {posts.map(post => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
